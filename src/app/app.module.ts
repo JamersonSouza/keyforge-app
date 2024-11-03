db362edd1b1374e3d5c34cd37bd63d6c9d0ab4b0
jamersonsouza <jamersonsouza@outlook.com>
2024-11-03 09:01:44 -0300

    feat 💄: inclusão do component home e formulários reativos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/keyforge-generate/pages/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { HomeComponent } from './modules/keyforge-generate/pages/home/home.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
