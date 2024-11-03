035f64a64d64105a969cf216d54918e7714affd8
jamersonsouza <jamersonsouza@outlook.com>
2024-11-03 19:07:50 -0300

    feat 💄: implementação do componente da home

--- src/app/modules/keyforge-generate/pages/home/home.component.ts
+++ src/app/modules/keyforge-generate/pages/home/home.component.ts
@@ -1,4 +1,8 @@
+import { KeyforgeServiceService } from './../../services/keyforge-service.service';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { KeyforgeData } from '../../model/keyforge-data';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +10,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  passwordForm: FormGroup;
+  generatedPassword: string = '';
+  keyforgeData! : KeyforgeData;
+  isLoading: boolean = false;
+  showGeneratedPassword: boolean = false;
+
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private keyForgeService : KeyforgeServiceService,
+    private msg : MessageService
+  ) {
+    this.passwordForm = this.formBuilder.group({
+      passwordLength: [''],
+      includeUppercaseLetters: [false],
+      includeLowercaseLetters: [false],
+      includeNumbers: [false],
+      includeSpecialCharacters: [false]
+    });
+  }
+
+
+  generatePassword(): void {
+   this.isLoading = true;
+    this.keyforgeData = this.passwordForm.value;
+    this.keyForgeService.generatePassword(this.keyforgeData).subscribe({
+      next: res => {
+        this.msg.add({severity: 'success', summary: 'Sucesso', detail: 'Senha gerada!'});
+        this.showGeneratedPassword = true;
+        this.generatedPassword = res.generatedPassword;
+        this.isLoading = false;
+      },
+      error: err => {
+        this.msg.add({ severity: 'error', summary: 'Erro', detail: 'Ocorreu um erro ao gerar a senha.' });
+        this.isLoading = false;
+      }
+    });
+  }
+
+  isGeneratePasswordButtonEnabled(): boolean {
+    const { passwordLength, includeUppercaseLetters, includeLowercaseLetters, includeNumbers, includeSpecialCharacters } = this.passwordForm.value;
+    const isAnyCheckboxChecked =
+      includeUppercaseLetters ||
+      includeLowercaseLetters ||
+      includeNumbers ||
+      includeSpecialCharacters;
+    return passwordLength > 0 && isAnyCheckboxChecked;
+  }
+
+  clearPassword(): void{
+    this.showGeneratedPassword = false;
+  }
 
 }
