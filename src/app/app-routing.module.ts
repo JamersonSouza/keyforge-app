a75efa1de621e0e781ff7adb26b2c4b901c63fbe
jamersonsouza <jamersonsouza@outlook.com>
2024-11-03 19:46:18 -0300

    refactor ♻️: inclusão de nova rota para listagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/keyforge-generate/pages/home/home.component';
+import { ListComponent } from './modules/keyforge-generate/pages/list/list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path:'list',
+    component: ListComponent
   }
+  
 ];
 
 @NgModule({
